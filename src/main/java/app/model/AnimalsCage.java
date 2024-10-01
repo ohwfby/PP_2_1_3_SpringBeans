ca4db1113af1a94911e82a5308e4d79811e3e5c6
Mikhail Semenov <haruko298@gmail.com>
2024-10-01 12:40:49 +0300

    Исправил, время одинаковое

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -6,18 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-
     @Autowired
     @Qualifier("dog")
     private Animal animal;
     @Autowired
     private Timer timer;
-
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,10 @@
 package app.model;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope
 public class Timer {
     private Long nanoTime = System.nanoTime();
 
